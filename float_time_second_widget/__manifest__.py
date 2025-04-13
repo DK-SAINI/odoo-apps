abaa935ca7ecc61e35b9acff5911761f8f089908
DK-SAINI <dheerajsaini814@gmail.com>
2025-04-13 12:31:05 +0530

    [ADD] add new float_time_second_widget module

---
+++ float_time_second_widget/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Float Time Second Widget",
+    "version": "18.0.0.1",
+    "depends": ["base", "web"],
+    "author": "Dheeraj Chauhan",
+    "category": "Tools",
+    "license": "LGPL-3",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/demo_model_views.xml",
+    ],
+    "assets": {
+        "web.assets_backend": [
+            "float_time_second_widget/static/src/js/float_time_second.js",
+            "float_time_second_widget/static/src/xml/float_time_second_template.xml",
+        ],
+    },
+    "images": ["static/description/banner.png"],
+    "installable": True,
+    "application": False,
+}
